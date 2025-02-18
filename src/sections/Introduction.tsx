7d5da8fe176532759cc42cb6419f38f749098fca
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-18 20:25:28 +0530

    ADD: Created the static version of Introduction section

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,5 +1,25 @@
-const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+import Tag from "@/components/Tag";
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+    return (
+        <section className="py-28 lg:py-40">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Introducing FloState</Tag>
+                </div>
+                <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+                    <span>
+                        You&apos;re eager to harness the power of AI agents,{" "}
+                    </span>
+                    <span className="text-white/15">
+                        but you are tired of wrestling with complex code just to
+                        automate your agentic workflows.{" "}
+                    </span>
+                    <span className="text-lime-400 block">
+                        That&apos;s why we built FloState.
+                    </span>
+                </div>
+            </div>
+        </section>
+    );
 }
