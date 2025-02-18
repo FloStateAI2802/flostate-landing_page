bb581833a6278c7e738f6c106371b631e1f934a4
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-18 17:29:14 +0530

    ADD: Created a static version of Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,61 @@
+import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image
+                        src={designExample1Image}
+                        alt="design example 1 img"
+                    />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image
+                        src={designExample2Image}
+                        alt="design example 2 img"
+                    />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Adwait" />
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Kshitij" color="red" />
+                </div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ &nbsp;More than 1000 agents deployed
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+                    Impactful agents, created effortlessly
+                </h1>
+                <p className="text-xl text-center text-white/50 mt-8 max-w-2xl mx-auto">
+                    Empower anyone to create custom AI agents without code,
+                    transforming complex tasks into simple, API-driven
+                    workflows. Build intelligent solutions tailored to your
+                    needs, regardless of technical expertise.
+                </p>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-4 md:flex-1"
+                    />
+                    <Button
+                        variant="primary"
+                        size="sm"
+                        type="submit"
+                        className="whitespace-nowrap"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }
