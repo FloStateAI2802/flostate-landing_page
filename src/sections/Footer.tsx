36fa30a276fc33be6457201405764d2bceee2038
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-19 20:18:52 +0530

    ADD: Created footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import finalLogo from "@/assets/images/final-shot-logo.svg";
+import Image from "next/image";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,32 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <Image
+                            src={finalLogo}
+                            alt="FloState logo"
+                            className="size-40 -my-[50px]"
+                        />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    href={link.href}
+                                    key={link.label}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
