07a83051dfa8c97e687e4f0d4cd5092c68942bbc
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-21 19:38:38 +0530

    ADD: Completed animations for CTA section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,51 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+
 export default function CallToAction() {
+    const [isHovered, setIsHovered] = useState(false);
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            { x: "-50%" },
+            { duration: 30, ease: "linear", repeat: Infinity }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) {
+            if (isHovered) {
+                animation.current.speed = 0.5;
+            } else {
+                animation.current.speed = 1;
+            }
+        }
+    }, [isHovered]);
+
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-6xl md:text-7xl font-medium">
+                <motion.div
+                    ref={scope}
+                    className="flex flex-none gap-16 pr-16 text-6xl md:text-7xl font-medium group cursor-pointer"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
                     {Array.from({ length: 10 }).map((_, i) => (
                         <div className="flex items-center gap-16" key={i}>
-                            <span className="text-lime-400 text-7xl">
+                            <span className="text-lime-400 group-hover:text-white text-7xl">
                                 &#10038;
                             </span>
-                            <span>Start Building Today</span>
+                            <span className="group-hover:text-lime-400">
+                                Start Building Today
+                            </span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
