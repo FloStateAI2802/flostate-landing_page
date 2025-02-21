bfed9b8e16aaa340405d8210208d829401476304
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-21 15:57:09 +0530

    ADD: Finished with animations of Introduction section

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,24 +1,59 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { useScroll, useTransform } from "framer-motion";
+
 import Tag from "@/components/Tag";
+import { twMerge } from "tailwind-merge";
+
+const text =
+    "but you are tired of wrestling with complex code just to automate your agentic workflows, wasting valuable time and resources on technical hurdles.";
+
+const words = text.split(" ");
 
 export default function Introduction() {
+    const scrollTarget = useRef<HTMLDivElement>(null);
+    const { scrollYProgress } = useScroll({
+        target: scrollTarget,
+        offset: ["start end", "end end"],
+    });
+    const [currentWord, setCurrentWord] = useState(0);
+    const wordIndex = useTransform(scrollYProgress, [0, 1], [0, words.length]);
+
+    useEffect(() => {
+        wordIndex.on("change", (latest) => {
+            setCurrentWord(latest);
+        });
+    }, [wordIndex]);
+
     return (
         <section className="py-28 lg:py-40">
             <div className="container">
-                <div className="flex justify-center">
-                    <Tag>Introducing FloState</Tag>
+                <div className="sticky top-20 md:top-28 lg:top-40">
+                    <div className="flex justify-center">
+                        <Tag>Introducing FloState</Tag>
+                    </div>
+                    <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+                        <span>
+                            You&apos;re eager to harness the power of AI agents,{" "}
+                        </span>
+                        <span>
+                            {words.map((word, wordIndex) => (
+                                <span
+                                    key={wordIndex}
+                                    className={twMerge(
+                                        "transition duration-500 text-white/15",
+                                        wordIndex < currentWord && "text-white"
+                                    )}
+                                >{`${word} `}</span>
+                            ))}
+                        </span>
+                        <span className="text-lime-400 block">
+                            That&apos;s why we built FloState.
+                        </span>
+                    </div>
                 </div>
-                <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
-                    <span>
-                        You&apos;re eager to harness the power of AI agents,{" "}
-                    </span>
-                    <span className="text-white/15">
-                        but you are tired of wrestling with complex code just to
-                        automate your agentic workflows.{" "}
-                    </span>
-                    <span className="text-lime-400 block">
-                        That&apos;s why we built FloState.
-                    </span>
-                </div>
+                <div className="h-[150vh]" ref={scrollTarget}></div>
             </div>
         </section>
     );
